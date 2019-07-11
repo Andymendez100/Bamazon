57513f93367ec1fa0d15cd3f4ab778117a879efd
Andy <andymendez100@gmail.com>
2019-07-11 04:06:54 -0700

    Finished Challenge 1

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 // All npms
 const mysql = require("mysql");
 
+const inquirer = require('inquirer');
 // mysql npm connection
 const connection = mysql.createConnection({
   host: "localhost",
@@ -17,16 +18,66 @@ const connection = mysql.createConnection({
 // connecting to mysql sever
 connection.connect(err => {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
   start();
 })
 
 // function that will show user products avaible and ask for id of product they would like to buy
-
 start = () => {
   showAllProducts();
-  
-}
+  connection.query("SELECT * FROM products", function (err, results) {
+    // Asking the user what item they would like and how many they want
+    inquirer
+      .prompt([{
+        name: "item",
+        type: "rawlist",
+        choices: () => {
+          // Putting all options from the database into an array
+
+          let choiceArray = [];
+
+          for (let i = 0; i < results.length; i++) {
+            choiceArray.push(results[i].item_id);
+          }
+          // Showing those options to the user
+          return choiceArray;
+        },
+        message: "What is the id of the product you would like to buy?"
+      }, {
+        name: 'quantity',
+        type: 'input',
+        message: 'How many units would you like of this product? '
+      }]).then(answer => {
+        
+        // Saving the infomation of the user's chosen id
+
+        let chosenItem;
+        for (let i = 0; i < results.length; i++) {
+          if (results[i].item_id === answer.item) {
+            chosenItem = results[i];
+          }
+        }
+        // If user is trying to order more than what is in stock
+        if (chosenItem.stock_quantity < answer.quantity) {
+          console.log(`Insufficient quantity! All i have in stock is ${chosenItem.stock_quantity} `);
+
+        } else {
+          connection.query('UPDATE products SET ? WHERE ?', [{
+            stock_quantity: chosenItem.stock_quantity - answer.quantity
+          }, {
+            item_id: answer.item
+          }],
+            function (error) {
+              if (error) throw err;
+              console.log(`Order was succesfully purchased`);
+              console.log(`Your total is: $${chosenItem.price * answer.quantity}`);
+            }
+          )
+        }
+        connection.end();
+      })
+  })
+};
+
 
 // Showing all products available
 showAllProducts = () => {
@@ -38,9 +89,6 @@ showAllProducts = () => {
       console.log(`Product Name: ${res[i].product_name}`);
       console.log(`Price: $${res[i].price}`);
       console.log("-----------------------------------");
-
     }
   });
 }
-
-
